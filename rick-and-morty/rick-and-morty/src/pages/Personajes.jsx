dfc1ab2a625cd64af668abc0d0460d8c5425ebb6
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-17 15:22:47 +0100

    styles btn

--- rick-and-morty/rick-and-morty/src/pages/Personajes.jsx
+++ rick-and-morty/rick-and-morty/src/pages/Personajes.jsx
@@ -4,7 +4,7 @@ import { ListaPersonajes } from '@/components/ListaPersonajes'
 export const Personajes = () => {
     return (<div>
         <audio autoPlay loop>
-            <source src="./music/Ivan Cornejo - Mirada (Official Lyric Video).mp3" type="audio/mp3" />
+            <source src="/music/Chaos-chaos.mp3" type="audio/mp3" />
         </audio>
 
         <h2 className="Personajes-title">Personajes</h2>
