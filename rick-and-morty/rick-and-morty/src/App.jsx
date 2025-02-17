bc44c9adb289ffc772e676f36ed2b32909890fb6
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-17 23:50:03 +0100

    router

--- rick-and-morty/rick-and-morty/src/App.jsx
+++ rick-and-morty/rick-and-morty/src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from 'react'
-import {ListaPersonajes} from './components/ListaPersonajes'
+import { ListaPersonajes } from './components/ListaPersonajes'
 import '@/css/personajes.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom';
 
-import {Lugares} from '@/pages/Lugares'
-import {Personajes} from '@/pages/Personajes'
-import {Episodios} from '@/pages/Episodios'
+
+import { Lugares } from '@/pages/Lugares'
+import { Personajes } from '@/pages/Personajes'
+import { Episodios } from '@/pages/Episodios'
 
 
 
 const NavBar = () => (
   <>
-  <div>
-    <img src="/img/rick-logo.png" alt="" />
-  </div>
-  <nav className='Navegacion'>
-    <ul className='Navegacion-ul'>
-      <li><a className='Navegacion-a' href="/personajes">Personajes</a></li>
-      <li><a className='Navegacion-a' href="/lugares">Lugares</a></li>
-      <li><a className='Navegacion-a' href="/episodios">Episodios</a></li>
-    </ul>
-  </nav>
-  
+    <div>
+      <img src="/img/rick-logo.png" alt="" />
+    </div>
+    <nav className='Navegacion'>
+      <ul className='Navegacion-ul'>
+        <li><NavLink className='Navegacion-a' to="/personajes">Personajes</NavLink></li>
+        <li><NavLink className='Navegacion-a' to="/lugares">Lugares</NavLink></li>
+        <li><NavLink className='Navegacion-a' to="/episodios">Episodios</NavLink></li>
+      </ul>
+    </nav>
+
   </>
 );
 
@@ -29,28 +32,40 @@ const NavBar = () => (
 
 function App() {
 
-  const pathname = window.location.pathname;
-  let Component;
+  // const pathname = window.location.pathname;
+  // let Component;
 
-  switch (pathname) {
-    case "/personajes":
-      Component = Personajes;
-      break;
-    case "/lugares":
-      Component = Lugares;
-      break;
-    case "/episodios":
-      Component = Episodios;
-      break;
-    default:
-      Component = Personajes;
-  }
+  // switch (pathname) {
+  //   case "/personajes":
+  //     Component = Personajes;
+  //     break;
+  //   case "/lugares":
+  //     Component = Lugares;
+  //     break;
+  //   case "/episodios":
+  //     Component = Episodios;
+  //     break;
+  //   default:
+  //     Component = Personajes;
+  // }
 
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
 
-      <Component />
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Personajes />} />
+
+          <Route path='/personajes' element={<Personajes />} />
+          <Route path='/lugares' element={<Lugares />} />
+          <Route path='/episodios' element={<Episodios />} />
+        </Routes>
+
+      </BrowserRouter>
+
+
+      {/* <Component /> */}
     </>
   )
 }
